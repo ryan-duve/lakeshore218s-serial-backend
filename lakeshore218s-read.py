8d4332496e5e85457526fdd4bdaa5a3542066d14
Ryan Duve <duve@virginia.edu>
2014-07-27 17:51:01 -0400

    write test to database

--- lakeshore218s-read.py
+++ lakeshore218s-read.py
@@ -2,6 +2,8 @@
 #lakeshore218s-read.py
 
 import serial
+import sys
+import mysql.connector
 
 ser = serial.Serial('/dev/ttyS0',
 	 9600,
@@ -17,4 +19,31 @@ ser.write("KRDG? 0\r\n")
 
 print(ser.read(64))
 
-ser.close()
+#mysql database stuff
+
+#get password from file
+with open ("password", "r") as myfile:
+    password=myfile.read().replace('\n', '')
+
+
+
+#http://dev.mysql.com/doc/connector-python/en/connector-python-connectargs.html
+config={
+	'user':'uva_remote',
+	'password':password,
+	'host':'hfgx620.tunl.daq',
+	'database':'slowcontrols',
+}
+
+cnx = mysql.connector.connect(**config)
+cur = cnx.cursor()
+
+query = "INSERT INTO usb1608g (device, raw_reading, measurement_reading) VALUES (%s,%s,%s)"
+data = ('test',1.0,2.0)
+
+cur.execute(query,data)
+cnx.commit()
+
+cur.close()
+cnx.close()
+
